917badccb63da37af848386d2315910428bdd44e
Mohammed Ali <mohaali482@gmail.com>
2022-07-27 21:29:34 +0300

    Add tooltip for the icon buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,8 @@ import TableRow from '@mui/material/TableRow';
 import InfoIcon from '@mui/icons-material/Info';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
@@ -51,9 +53,21 @@ const Home = () => {
                 2021-2-2
               </StyledTableCell>
               <StyledTableCell align='center'>
-                  <InfoIcon sx={{color:'custom.main', cursor:'pointer', mr:'.2em'}} />
-                  <EditIcon sx={{color:'secondary.main', cursor:'pointer', mr:'.2em'}} />
-                  <DeleteIcon sx={{color:'primary.danger', cursor:'pointer', mr:'.2em'}}/>
+                <Tooltip title='Detail'>
+                  <IconButton>
+                    <InfoIcon sx={{color:'custom.main'}} />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title='Edit'>
+                  <IconButton>
+                    <EditIcon sx={{color:'secondary.main'}} />
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title='Delete'>
+                  <IconButton>
+                    <DeleteIcon sx={{color:'primary.danger'}}/>
+                  </IconButton>
+                </Tooltip>
               </StyledTableCell>
           </TableBody>
         </Table>

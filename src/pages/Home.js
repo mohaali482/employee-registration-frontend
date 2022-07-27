af582c5798d50df5b95c115d69313a6bb5e52712
Mohammed Ali <mohaali482@gmail.com>
2022-07-27 21:10:14 +0300

    Add Table for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,64 @@
-import React from 'react'
+import React from 'react';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import InfoIcon from '@mui/icons-material/Info';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    color: theme.palette.custom.contrastText,
+  }
+}));
 
 const Home = () => {
   return (
-    <React.Fragment>
-        <div>Home</div>
-    </React.Fragment>
+    <Container sx={{mt: '2em'}}>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow sx={{backgroundColor:'custom.main'}}>
+              <StyledTableCell>
+                No.
+              </StyledTableCell>
+              <StyledTableCell align='left'>
+                Full Name
+              </StyledTableCell>
+              <StyledTableCell align='center'>
+                Created Date
+              </StyledTableCell>
+              <StyledTableCell align='center'>
+                Actions
+              </StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+              <StyledTableCell>
+                1.
+              </StyledTableCell>
+              <StyledTableCell align='left'>
+                Abebe Beso
+              </StyledTableCell>
+              <StyledTableCell align='center'>
+                2021-2-2
+              </StyledTableCell>
+              <StyledTableCell align='center'>
+                  <InfoIcon sx={{color:'custom.main', cursor:'pointer', mr:'.2em'}} />
+                  <EditIcon sx={{color:'secondary.main', cursor:'pointer', mr:'.2em'}} />
+                  <DeleteIcon sx={{color:'primary.danger', cursor:'pointer', mr:'.2em'}}/>
+              </StyledTableCell>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   )
 }
 

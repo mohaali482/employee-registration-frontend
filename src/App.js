e11a480aa0a6c278ccc09ae18066960b26c734da
Mohammed Ali <mohaali482@gmail.com>
2022-07-29 19:59:02 +0300

    Add employee add page into route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Add from './pages/Add';
 import Detail from './pages/Detail';
 import Home from './pages/Home';
 import Container from '@mui/material/Container';
@@ -12,6 +13,7 @@ const App = () => {
         <Container sx={{mt: '2em'}}>
           <Routes>
               <Route exact path='/detail/:id' element={ <Detail /> }/>
+              <Route exact path='/add' element={ <Add /> }/>
               <Route exact path='/' element={ <Home /> }/>
           </Routes>
         </Container>

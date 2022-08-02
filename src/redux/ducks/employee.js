d153608309ca8b56fc0b62d10a69678d826d0d75
Mohammed Ali <mohaali482@gmail.com>
2022-08-02 18:43:33 +0300

    Add an employee duck

---
+++ src/redux/ducks/employee.js
@@ -0,0 +1,71 @@
+const CREATE = "create";
+const UPDATE = "update";
+const DELETE = "delete";
+
+export const createEmployee = (data) => ({
+    type: CREATE,
+    payload: {...data}
+})
+
+export const updateEmployee = (employee) => ({
+    type: UPDATE,
+    payload: {...employee}
+})
+
+export const deleteEmployee = (id) => ({
+    type: DELETE,
+    payload: id
+})
+
+const nextId = (employees) => {
+    let max = 0;
+    employees.map((employee) => {
+        if (max > employee.id){
+            max = employee.id
+        }
+    })
+
+    return max++;
+}
+
+const initialState = {
+    employees:[]
+}
+
+export default function reducer(state=initialState, action) {
+    switch (action.type) {
+        case CREATE:
+            return {
+                ...state,
+                employees: [
+                    state.employees,
+                    {
+                        id: nextId(state),
+                        ...action.payload
+                    }
+                ]
+            }
+        case UPDATE:
+            return {
+                ...state,
+                employees: [
+                    state.employees,
+                    {
+                        id: action.payload.id,
+                        ...action.payload
+                    }
+                ]
+            }
+
+        case DELETE:
+            return {
+                ...state,
+                employees: [
+                    state.employees.filter((employee) => employee.id !== action.payload)
+                ]
+            }
+
+        default:
+            break;
+    }
+}

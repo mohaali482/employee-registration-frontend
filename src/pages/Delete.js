7dac1a3a73198a8c5971304ca3d16182a2ad56f6
Mohammed Ali <mohaali482@gmail.com>
2022-08-03 20:13:14 +0300

    Update delete to connect to store

--- src/pages/Delete.js
+++ src/pages/Delete.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { Button, Card, CardActions, CardContent, CardHeader, Typography } from '@mui/material';
+import { deleteEmployee } from '../redux/ducks/employee';
 
 const Delete = () => {
     const navigate = useNavigate();
-    const handleDelete = (e) => {
-        // Delete method here.
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const { employees } = useSelector((state) => state.employees)
+    const employee = employees.find((employee) => employee.id == id)
+
+    const handleDelete = () => {
+        dispatch(deleteEmployee(id))
+        navigate('/')
     }
   return (
     <Card>
         <CardHeader title={'Delete Employee'} sx={{backgroundColor: 'danger.main', color: 'danger.contrastText'}}/>
+        {
+            employee !== undefined ?
+        (<>
         <CardContent>
             <Typography variant='h5' component='p'>
                 Do you really want to delete this employee?
@@ -19,6 +30,20 @@ const Delete = () => {
             <Button color='danger' variant='contained' onClick={handleDelete}>Yes</Button>
             <Button color='secondary' variant='contained' onClick={() => navigate(-1)}>No</Button>
         </CardActions>
+        </>) :
+        (
+            <>
+            <CardContent>
+                <Typography variant='h5' component='p'>
+                    Employee not found.
+                </Typography>
+            </CardContent>
+            <CardActions sx={{float: 'right'}}>
+                <Button color='secondary' variant='contained' onClick={() => navigate(-1)}>Back</Button>
+            </CardActions>
+            </>
+        )
+    }
     </Card>
   )
 }

8189a294c5ee12eb243966257542caa1e2c96ec6
Mohammed Ali <mohaali482@gmail.com>
2022-08-08 18:56:59 +0300

    Add the handler to the root saga

--- src/redux/sagas/rootsaga.js
+++ src/redux/sagas/rootsaga.js
@@ -1,7 +1,18 @@
-import { takeLatest } from 'redux-saga/effects';
-import { GET_EMPLOYEE_LIST } from '../ducks/employee';
-import { handleGetEmployeeList } from './handlers/employee';
+import { takeLatest, all } from 'redux-saga/effects';
+import { GET_EMPLOYEE, GET_EMPLOYEE_LIST } from '../ducks/employee';
+import { handleGetEmployee, handleGetEmployeeList } from './handlers/employee';
+
+export function* getEmployeeListSaga(){
+    yield takeLatest(GET_EMPLOYEE_LIST, handleGetEmployeeList)
+}
+
+export function* getEmployeeSaga(){
+    yield takeLatest(GET_EMPLOYEE, handleGetEmployee)
+}
 
 export function* watcherSaga(){
-    yield takeLatest(GET_EMPLOYEE_LIST, handleGetEmployeeList)
+    yield all([
+        getEmployeeListSaga(),
+        getEmployeeSaga()
+    ])
 }

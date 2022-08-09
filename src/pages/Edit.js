1756a67b8f6d862fee677482c5c1156e806c0b85
Mohammed Ali <mohaali482@gmail.com>
2022-08-09 21:43:16 +0300

    Update the edit page to get data from the API

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux/es/exports';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import InputForm from '../components/InputForm';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { getEmployee, updateEmployee } from '../redux/ducks/employee';
 
 
 const Edit = () => {
-    const { employees } = useSelector((state) => state.employees);
-    const { id } = useParams();
-    const employee = employees.find((employee) => employee.id == id)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { id } = useParams();
+  useEffect(()=>{
+    dispatch(getEmployee(id));
+  }, [dispatch])
+  const { employee } = useSelector((state) => state.employees)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = {
+      id,
+      form: event.target
+    }
+    dispatch(updateEmployee(data))
+    navigate(`/detail/${id}`)
+  }
   return (
     <Card>
         <CardHeader title={`Update Employee`} sx={{backgroundColor: 'secondary.main', color: '#fff'}}/>
-        <InputForm employee={employee} update={true}/>
+        {employee && <InputForm employee={employee} handleSubmit={handleSubmit}/>}
     </Card>
   )
 }

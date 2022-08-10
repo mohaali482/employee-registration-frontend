fa3b9d8f28472ffbd61c829f3b858ac9c590ead3
Mohammed Ali <mohaali482@gmail.com>
2022-08-10 18:21:43 +0300

    Add a redux saga for the delete employee action

--- src/redux/sagas/rootsaga.js
+++ src/redux/sagas/rootsaga.js
@@ -1,6 +1,6 @@
 import { takeLatest, all } from 'redux-saga/effects';
-import { GET_EMPLOYEE, GET_EMPLOYEE_LIST, UPDATE_EMPLOYEE } from '../ducks/employee';
-import { handleGetEmployee, handleGetEmployeeList, handleUpdateEmployee } from './handlers/employee';
+import { DELETE_EMPLOYEE, GET_EMPLOYEE, GET_EMPLOYEE_LIST, UPDATE_EMPLOYEE } from '../ducks/employee';
+import { handleDeleteEmployee, handleGetEmployee, handleGetEmployeeList, handleUpdateEmployee } from './handlers/employee';
 
 export function* getEmployeeListSaga(){
     yield takeLatest(GET_EMPLOYEE_LIST, handleGetEmployeeList)
@@ -14,10 +14,15 @@ export function* updateEmployeeSaga(){
     yield takeLatest(UPDATE_EMPLOYEE, handleUpdateEmployee)
 }
 
+export function* deleteEmployeeSaga(){
+    yield takeLatest(DELETE_EMPLOYEE, handleDeleteEmployee)
+}
+
 export function* watcherSaga(){
     yield all([
         getEmployeeListSaga(),
         getEmployeeSaga(),
         updateEmployeeSaga(),
+        deleteEmployeeSaga(),
     ])
 }

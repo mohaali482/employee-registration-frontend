edd976e6d6635e4f1359b7d0352eceb67ce13544
Mohammed Ali <mohaali482@gmail.com>
2022-08-10 19:33:40 +0300

    Connect the add page to the API

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -2,13 +2,27 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import InputForm from '../components/InputForm';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux/es/exports';
+import { createEmployee } from '../redux/ducks/employee';
 
 
 const Add = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    const data = {
+      form: event.target
+    }
+    dispatch(createEmployee(data))
+    navigate('/')
+  }
   return (
     <Card>
         <CardHeader title={`Add a new Employee`} sx={{backgroundColor: 'secondary.main', color: '#fff'}}/>
-        <InputForm />
+        <InputForm handleSubmit={handleSubmit}/>
     </Card>
   )
 }

03681f73db4df6942e334a2e6f3ffefcd262f3f3
Mohammed Ali <mohaali482@gmail.com>
2022-08-15 14:15:26 +0300

    Add form validation for the add page

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import InputForm from '../components/InputForm';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux/es/exports';
-import { createEmployee } from '../redux/ducks/employee';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { createEmployee, setEmployeeNull, setFormErrorsNull } from '../redux/ducks/employee';
+
 
 
 const Add = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { formErrors } = useSelector(state => state.employees)
+  const { employee } = useSelector(state => state.employees)
 
   const handleSubmit = (event)=>{
     event.preventDefault();
+    if (event.target.image.value === ''){
+      alert('Upload an Image')
+      return
+    }
     const data = {
       form: event.target
     }
     dispatch(createEmployee(data))
-    navigate('/')
   }
+  useEffect(()=>{
+    if (Object.keys(employee).length > 0){
+      navigate('/')
+    }
+  }, [employee, navigate])
+
+  useEffect(()=>{
+    return () =>{
+      dispatch(setFormErrorsNull());
+      dispatch(setEmployeeNull());
+    }
+  }, [dispatch])
+
   return (
     <Card>
         <CardHeader title={`Add a new Employee`} sx={{backgroundColor: 'secondary.main', color: '#fff'}}/>
-        <InputForm handleSubmit={handleSubmit}/>
+        <InputForm handleSubmit={handleSubmit} formErrors={formErrors}/>
     </Card>
   )
 }
